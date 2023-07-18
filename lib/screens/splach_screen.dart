e8e4285f57de0f7241ca280b1917804607e8ea4a
bayrem999 <bayrem.hamdi@esprit.tn>
2023-07-18 11:27:23 +0100

    UI_update : splash_screen

--- lib/screens/splach_screen.dart
+++ lib/screens/splach_screen.dart
@@ -1,9 +1,11 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:lottie/lottie.dart';
 import 'package:sign_in/screens/Home/home_screen.dart';
 import 'package:sign_in/screens/Wrapper.dart';
 
-
+/*
 class splach extends StatefulWidget {
   const splach({super.key});
 
@@ -66,4 +68,21 @@ decoration: BoxDecoration(
       ),
     );
   }
+}*/
+
+class splach extends StatelessWidget {
+  const splach({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedSplashScreen(
+        splash: Lottie.asset("assets/animations/access.json"),
+
+        splashIconSize: 300,
+        backgroundColor: Colors.white,
+        splashTransition: SplashTransition.fadeTransition,
+        animationDuration: const Duration(seconds: 2),
+        nextScreen: const wrapper());
+  }
 }
+
